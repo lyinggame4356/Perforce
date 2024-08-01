ac41073edb77dd0a8733527b64187b1cfc42843b
lyinggame4356 <158070999+lyinggame4356@users.noreply.github.com>
2024-08-01 17:16:20 +0300

    add new helper functions

---
+++ fizzbuzz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    int i;
+
+    for (i = 1; i <= 100; i++)
+        if (i % 3 == 0 || i % 5 == 0) 
+            printf("%s%s\n", 
+                i%3==0 ? "Fizz":"", 
+                i%5==0 ? "Buzz":"" 
+            );
+        else 
+            printf("%d\n", i);
+
+    return 0;
+}
